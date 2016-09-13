81f5d464e8331929943b65d8011f17ff3767ab0f
BuildTools <grucemax@uw.edu>
2016-09-12 19:42:12 -0700

    Added large database version. Reworked sortable_version for future word UUID(using prime). Fixed net casting early problem. Found some cool ways to measure progress in indexing words/cmds.

--- joker/jokerCommandNode.py
+++ joker/jokerCommandNode.py
@@ -14,15 +14,54 @@ class JokerCommandNode:
         return self.cmd + ":" + str(self.value)
 
 
+def _sortable_score(string):
+    score = 0.0
+    for letter in "etao":
+        score += string.count(letter) * letter_common_score(letter)
+    return score + len(string)
+
+
 def sortable_score(string):
     score = 0.0
+    length = len(string)
     for letter in string:
-        score += letter_common_score(letter)
-    return score + len(string)
+        temp_letter_score = letter_common_score(letter)
+        score += prime_eq(temp_letter_score)
+    return score + length
+
+
+def __sortable_score(string):
+    score = 0.0
+    length = len(string)
+    for letter in string:
+        temp_letter_score = letter_common_score(letter)
+        score += temp_letter_score
+    return score + length
 
 
 def letter_common_score(letter):
     return {'a': 24, 'b': 7, 'c': 15, 'd': 17, 'e': 26, 'f': 11,
             'g': 10, 'h': 19, 'i': 22, 'j': 4, 'k': 5, 'l': 16, 'm': 13,
             'n': 21, 'o': 23, 'p': 8, 'q': 2, 'r': 18, 's': 20, 't': 25,
-            'u': 14, 'v': 6, 'w': 12, 'x': 3, 'y': 9, 'z': 1, ' ': 0}.get(letter)
+            'u': 14, 'v': 6, 'w': 12, 'x': 3, 'y': 9, 'z': 1, ' ': 0,
+            '1': 1, '2': 1, '3': 1, '4': 1, '5': 1, '6': 1, '7': 1,
+            '8': 1, '9': 1, '0': 1, '-': 1, '.': 1, ',': 1, '\'': 1,
+            '/': 1, '&': 1, '!': 1}.get(letter)
+
+
+def prime_eq(num):
+    return {1: 2, 2: 3, 3: 5, 4: 7, 5: 11, 6: 13, 7: 17, 8: 19, 9: 23, 10: 29,
+            11: 31, 12: 37, 13: 41, 14: 43, 15: 47, 16: 53, 17: 59, 18: 61,
+            19: 67, 20: 71, 21: 73, 22: 79, 23: 83, 24: 89, 25: 97, 26: 101,
+            27: 103, 28: 107, 29: 109, 30: 113, 31: 127, 32: 131, 33: 137, 34: 139,
+            35: 149, 36: 151, 37: 157, 38: 163, 39: 167, 40: 173, 41: 179, 42: 181,
+            43: 191, 0: 193}.get(num)
+
+
+def prime_eq_two(num):
+    return {1: 197, 2: 199, 3: 211, 4: 223, 5: 227, 6: 229, 7: 233, 8: 239, 9: 241,
+            10: 251, 11: 257, 12: 263, 13: 269, 14: 271, 15: 277, 16: 281, 17: 283,
+            18: 293, 19: 307, 20: 311, 21: 313, 22: 317, 23: 331, 24: 337, 25: 347,
+            26: 349, 27: 353, 28: 359, 29: 367, 30: 373, 31: 379, 32: 383, 33: 389,
+            34: 397, 35: 401, 36: 409, 37: 419, 38: 421, 39: 431, 40: 433, 41: 439,
+            42: 443, 43: 449, 0: 457}.get(num)
--- joker/main.py
+++ joker/main.py
@@ -1,7 +1,8 @@
-from joker.jokerCommandNode import JokerCommandNode
+from joker.jokerCommandNode import sortable_score, JokerCommandNode
 from joker.jokerCommandTree import JokerCommandTree
-from joker.jokerUtil import filter_and_pick_joker
-import enchant
+from joker.jokerUtil import filter_and_pick_joker, word_dic_net, filter_and_pick_joker_str
+from sys import setrecursionlimit
+from time import time
 
 welcome_cmd_s = "welcome"
 exit_cmd_s = "leave"
@@ -9,6 +10,10 @@ exit_cmd_s = "leave"
 
 def get_joker_cmds():
     return {"print name": print_name, welcome_cmd_s: welcome, exit_cmd_s:leave}
+
+
+def word_list():
+    return open("words.txt").read().split("\n")
 
 
 def print_name():
@@ -35,23 +40,70 @@ def main():
 
     while in_cmd is not exit_cmd_s:
         s_cmd = input("Welcome! Please enter a command, peasant. ")
-        net = cmd_tree.find_similar_net(s_cmd, 30.0, 10)
+        net = cmd_tree.find_similar_net(s_cmd, 20.0, -1)
         in_cmd = filter_and_pick_joker(s_cmd, net)
         cmd_reference[in_cmd]()
 
 
 def diction():
-    cmd_tree = JokerCommandTree()
-    cmd_reference = get_joker_cmds()
-    for cmd in cmd_reference.keys():
-        if cmd is not None:
-            cmd_tree.add(cmd)
+    setrecursionlimit(150000)
+    word_tree = JokerCommandTree()
+    start_time = time() * 1000
+    word_reference = word_list()
+    length = len(word_reference)
+    count = 0
+    for word in word_reference:
+        temp_time = time() * 1000
+        if word is not None:
+            word_tree.add(word.lower())
+        count += 1
+        if (temp_time - start_time) >= 1000:
+            start_time = time() * 1000
+            print("Indexed " + str(count))
+            count = 0
 
-    in_cmd = welcome_cmd_s
+    print("Words loaded.\n")
+    end_time = time() * 1000
+    print("It took: " + str(end_time - start_time))
 
-    while in_cmd is not exit_cmd_s:
+    in_word = "none"
+
+    while in_word is not "leave":
         s_cmd = input("Try and spell a word, and I will guess what it is. ")
-        net = cmd_tree.find_similar_net(s_cmd, 30.0, 10)
-        in_cmd = filter_and_pick_joker(s_cmd, net)
-        print("Are you trying to spell " + in_cmd + "?")
-main()
+        net = word_tree.find_similar_net(s_cmd, 0, 2)
+        in_word = filter_and_pick_joker(s_cmd, net)
+        print("Are you trying to spell " + in_word + "?")
+
+
+def diction_with_dic():
+    word_dic = {}
+    start_time = time() * 1000
+    word_reference = word_list()
+    count = 0
+    for word in word_reference:
+        temp_time = time() * 1000
+        if word is not None:
+            temp_score = sortable_score(word.lower())
+            if word_dic.get(temp_score) is None:
+                word_dic[temp_score] = [word.lower()]
+            else:
+                word_dic[temp_score].append(word.lower())
+        count += 1
+        if (temp_time - start_time) >= 2000:
+            start_time = time() * 1000
+            print("Indexed " + str(count))
+            count = 0
+
+    print("Words loaded.\n")
+    end_time = time() * 1000
+    print("It took: " + str(end_time - start_time))
+    print("The algorithm lost " + str(len(word_reference) - len(word_dic)) + " words")
+
+    in_word = "none"
+
+    while in_word is not "leave":
+        s_cmd = input("Try and spell a word, and I will guess what it is. ")
+        net = word_dic_net(s_cmd, word_dic, 10.0, 100000)
+        in_word = filter_and_pick_joker_str(s_cmd, net)
+        print("Are you trying to spell " + in_word + "?")
+diction_with_dic()
